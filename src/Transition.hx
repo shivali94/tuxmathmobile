cefd56a7c9fb3bc9c28b50ba9cfc12c593a88a7e
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2012-08-10 02:51:16 +0530

    Some performance improvement in transition animation

--- src/Transition.hx
+++ src/Transition.hx
@@ -14,12 +14,18 @@ class Transition
 {
 	static var star_field:StarField;
 	static var capture:BitmapData;
+	static var pre_image:Bitmap ;
+	static var pos_trans:BitmapData;
+	static var pos_image:Bitmap;
 	static public var dispatch:EventDispatcher;
 	static public var TRANSITION_COMPLETE:String;
 	public static function intialize()
 	{
 		star_field = new StarField();
 		capture = new BitmapData(GameConstant.stageWidth, GameConstant.stageHeight);
+		pre_image = new Bitmap(capture);
+		pos_trans = new BitmapData(GameConstant.stageWidth, GameConstant.stageHeight);
+		pos_image = new Bitmap(pos_trans);
 		dispatch = new EventDispatcher();
 		TRANSITION_COMPLETE = "transition completed";
 	}
@@ -28,7 +34,6 @@ class Transition
 		star_field.alpha = 1;
 		star_field.visible = true;
 		capture.draw(Lib.current.stage);
-		var pre_image:Bitmap = new Bitmap(capture);
 		// Removing all image and sprites that are to be remove 
 		for (x in 0...remove.length)
 			target.removeChild(remove[x]);
@@ -37,9 +42,8 @@ class Transition
 			target.addChild(add[x]);
 		
 		// Capturing image for post transition
-		var pos_trans:BitmapData = new BitmapData(target.width, target.height);
+		
 		pos_trans.draw(target);
-		var pos_image:Bitmap = new Bitmap(pos_trans);
 		pos_image.scaleX = pos_image.scaleY = 2.5;
 		pos_image.x = -GameConstant.stageWidth * (pos_image.scaleX-1) / 2;
 		pos_image.y = -GameConstant.stageHeight * (pos_image.scaleY - 1) / 2;
@@ -48,11 +52,16 @@ class Transition
 		//Adding starfield 
 		target.addChild(star_field);
 		//Adding captured screen
+		pre_image.visible = true;
+		pre_image.scaleX = pre_image.scaleY = 1;
+		pre_image.x = pre_image.y = 0;
+		pre_image.alpha = 1;
 		target.addChild(pre_image);
+		
 		//Animation
 		// Zomming captured image. 
 		var temp_instance = GameConstant.space_travel.play();						// starting space_travel time sound 
-		Actuate.tween(pre_image, 1, { scaleX:3, scaleY:3, alpha:0 } ).onUpdate(function() {
+		Actuate.tween(pre_image, 1.5, { scaleX:3, scaleY:3, alpha:0 } ).onUpdate(function() {
 			pre_image.x = -GameConstant.stageWidth * (pre_image.scaleX-1) / 2;
 			pre_image.y = -GameConstant.stageHeight * (pre_image.scaleY - 1) / 2;
 		}).onComplete(function() {			
